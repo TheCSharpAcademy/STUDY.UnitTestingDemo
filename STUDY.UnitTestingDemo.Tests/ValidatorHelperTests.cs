8e58f1768ba0b9288a294f214b8900f58e41f193
thecsharpacademy <thecsharpacademy@gmail.com>
2025-12-12 23:25:57 +1000

    Adding params

--- STUDY.UnitTestingDemo.Tests/ValidatorHelperTests.cs
+++ STUDY.UnitTestingDemo.Tests/ValidatorHelperTests.cs
@@ -2,13 +2,20 @@
 
 public class ValidatorHelperTests
 {
-    [Test]
-    public void CorrectInput_ReturnsTrue()
+    [TestCase("123", true, 123)]
+    [TestCase("123 ", true, 123)]
+    [TestCase(" 123", true, 123)]
+    [TestCase(" 123 ", true, 123)]
+    [TestCase("0", true, 0)]
+    public void CorrectInput_ReturnsTrue_AndResultingIntegerIsCorrect(
+       string input,
+       bool expectedResult,
+       int expectedValidatedValue)
     {
-        var isValidInteger = ValidatorHelper.IsValidInteger("50", out int validInteger);
+        var isValidInteger = ValidatorHelper.IsValidInteger(input, out int validInteger);
 
-        Assert.That(isValidInteger, Is.True);
-        Assert.That(validInteger, Is.EqualTo(50));
+        Assert.That(isValidInteger, Is.EqualTo(expectedResult));
+        Assert.That(validInteger, Is.EqualTo(expectedValidatedValue));
     }
 
     [Test]
