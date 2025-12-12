220f40a33253b550f78a150a1ae429a707a5b2da
thecsharpacademy <thecsharpacademy@gmail.com>
2025-12-12 23:32:56 +1000

    Expanded tests, removed negative test

--- STUDY.UnitTestingDemo.Tests/ValidatorHelperTests.cs
+++ STUDY.UnitTestingDemo.Tests/ValidatorHelperTests.cs
@@ -3,12 +3,53 @@
 public class ValidatorHelperTests
 {
     private static readonly object[] IsValidIntegerCases =
-    {
-        new TestCaseData("123",true,123),
-        new TestCaseData("123 ",true,123),
-        new TestCaseData(" 123",true,123),
-        new TestCaseData(" 123 ",true,123),
-        new TestCaseData("0",true,0)
+   {
+        new TestCaseData("123",      true,  123),
+        new TestCaseData("123 ",     true,  123),
+        new TestCaseData(" 123",     true,  123),
+        new TestCaseData(" 123 ",    true,  123),
+        new TestCaseData("0",        true,  0),
+        new TestCaseData("-50",      true,  -50),
+        new TestCaseData(" -50 ",    true,  -50),
+
+        new TestCaseData("",         false, 0),
+        new TestCaseData(" ",        false, 0),
+        new TestCaseData("   ",      false, 0),
+        new TestCaseData("\t",       false, 0),
+        new TestCaseData("\n",       false, 0),
+
+        new TestCaseData(null,       false, 0),
+
+        new TestCaseData("abc",      false, 0),
+        new TestCaseData("one",      false, 0),
+        new TestCaseData("minus five", false, 0),
+
+        new TestCaseData("123abc",   false, 0),
+        new TestCaseData("abc123",   false, 0),
+        new TestCaseData("12 3",     false, 0),
+        new TestCaseData("1 2 3",    false, 0),
+
+        new TestCaseData("!",        false, 0),
+        new TestCaseData("#$%",      false, 0),
+        new TestCaseData("12!",      false, 0),
+        new TestCaseData("--1",      false, 0),
+        new TestCaseData("++1",      false, 0),
+        new TestCaseData("+-1",      false, 0),
+        new TestCaseData("-",        false, 0),
+        new TestCaseData("+",        false, 0),
+
+        new TestCaseData("12.3",     false, 0),
+        new TestCaseData("-12.3",    false, 0),
+        new TestCaseData("1,234.00", false, 0),
+        new TestCaseData("1.0e3",    false, 0),
+        new TestCaseData("1e3",      false, 0),
+
+        new TestCaseData("1,234",       false, 0),
+        new TestCaseData("0x10",        false, 0),
+        new TestCaseData("0b1010",      false, 0),
+
+        new TestCaseData("2147483648",  false, 0),
+        new TestCaseData("-2147483649", false, 0),
     };
 
     [TestCaseSource(nameof(IsValidIntegerCases))]
@@ -21,14 +62,5 @@ public class ValidatorHelperTests
 
         Assert.That(isValidInteger, Is.EqualTo(expectedResult));
         Assert.That(validInteger, Is.EqualTo(expectedValidatedValue));
-    }
-
-    [Test]
-    public void CorrectNegativeInput_ReturnsTrue()
-    {
-        var isValidInteger = ValidatorHelper.IsValidInteger("-50", out int validInteger);
-
-        Assert.That(isValidInteger, Is.True);
-        Assert.That(validInteger, Is.EqualTo(-50));
     }
 }
