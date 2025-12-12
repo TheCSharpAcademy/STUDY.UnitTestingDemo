5da20257e1a7417ba1db9a049788970f8c33cfba
thecsharpacademy <thecsharpacademy@gmail.com>
2025-12-12 23:23:25 +1000

    Multiple tests and assertions

--- STUDY.UnitTestingDemo.Tests/ValidatorHelperTests.cs
+++ STUDY.UnitTestingDemo.Tests/ValidatorHelperTests.cs
@@ -8,5 +8,15 @@ public class ValidatorHelperTests
         var isValidInteger = ValidatorHelper.IsValidInteger("50", out int validInteger);
 
         Assert.That(isValidInteger, Is.True);
+        Assert.That(validInteger, Is.EqualTo(50));
+    }
+
+    [Test]
+    public void CorrectNegativeInput_ReturnsTrue()
+    {
+        var isValidInteger = ValidatorHelper.IsValidInteger("-50", out int validInteger);
+
+        Assert.That(isValidInteger, Is.True);
+        Assert.That(validInteger, Is.EqualTo(-50));
     }
 }
